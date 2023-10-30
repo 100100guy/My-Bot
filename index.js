e36eb2bde3864bb0277ada3be1d10dce02534f36
Md. Nafiu Rahman <nafiu.rahman@gmail.com>
2023-10-31 00:50:11 +0600

    working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import fetch from 'node-fetch';
 import gis from 'g-i-s';
-import Discord, { Client, Events, GatewayIntentBits } from 'discord.js';
+import Discord, { Client, Events, GatewayIntentBits, EmbedBuilder  } from 'discord.js';
 
 // Load environment variables from a .env file
 config();
@@ -13,22 +13,58 @@ if (config.error) {
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,
   GatewayIntentBits.GuildMembers,]
 });
 
+/**
+ * @param {Discord.Message} message
+ */
+
+async function getWeather(message) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Assam&appid=${WEATHER_API_KEY}&units=metric`);
+    const data = await response.json();
+
+    if (data.cod === 200) {
+      const temperature = data.main.temp;
+      const description = data.weather[0].description;
+      const humidity = data.main.humidity;
+
+      // Get the emoji for the weather description
+      const weatherIcon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+      // Create a plain text message
+      const weatherMessage = new EmbedBuilder()
+        .setTitle(`Weather in Assam`)
+        .addFields(
+          { name: 'Temperature', value: `${temperature}°C`, inline: true },
+          { name: 'Description', value: description , inline: true },
+          { name: 'Humidity', value: `${humidity}%`, inline: true },
+        )
+        .setThumbnail(weatherIcon); // Set the weather icon as a thumbnail
+      message.channel.send({ embeds: [weatherMessage] });
+    } else {
+      message.channel.send('Unable to fetch weather data for Assam.');
+    }
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    message.channel.send('Failed to fetch weather data for Assam.');
+  }
+}
 
 /**
  * @param {Discord.Message} message
  */
-async function GetMeme(message) {
+async function GetNews(message) {
   try {
     // Fetch news from London using NewsAPI
     const response = await fetch(`https://newsapi.org/v2/everything?q=Assam&apiKey=${NEWS_API_KEY}`);
     const newsData = await response.json();
-    
+
     if (newsData.articles && newsData.articles.length > 0) {
       // Select a random news article
       const randomIndex = Math.floor(Math.random() * newsData.articles.length);
@@ -36,7 +72,7 @@ async function GetMeme(message) {
 
       // Send the random news article as a message
       message.channel.send(`${randomArticle.title}\n${randomArticle.url}`);
-    }  else {
+    } else {
       message.channel.send("No news articles found for Assam.");
     }
   } catch (error) {
@@ -72,9 +108,11 @@ client.on("messageCreate", (message) => {
         "./dadjoke for a dadjoke 👀\n" +
         "./meme for a meme from r/memes");
     }
-    else if (tokens[0].toLowerCase() === "./memes") {
-      
-      GetMeme(message)
+    else if (tokens[0].toLowerCase() === "./news") {
+      GetNews(message)
+    }
+    else if (tokens[0].toLowerCase() === "./weather") {
+      getWeather(message)
     }
   }
 });
